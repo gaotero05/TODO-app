e923ff87b7f2df580c2b86704894d0507e6f850b
gaotero05 <gaotero05@gmail.com>
2026-03-27 18:17:29 -0400

    commit

--- App.js
+++ App.js
@@ -18,3 +18,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+
+  let data = [
+    {
+      key: "1",
+      title: "Title1"
+    },
+    {
+      key: "2",
+      title: "Title2"
+    }
+  ]
+
+  let renderItem = ({ item }) => {
+    return <Text>{item.title}</Text>
+  }
+  return (<SafeAreaView style={styles.container}>
+    <FlatList data={data} renderItem={renderItem}></FlatList>
+  </SafeAreaView>)
+}
